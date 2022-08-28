07594d744885f8adbfde339ffd83ad3bcabf9471
MrThong24 <thong.buichi@ncc.asia>
2022-08-28 19:05:22 +0700

    first commit

---
+++ thong.rb
@@ -0,0 +1,14 @@
+p "Xin chào Bùi Chí Thông - 1911505310263"
+puts "Nhập số bất kỳ"
+number = gets
+puts "Số vừa nhập là: #{number}"
+puts "So sánh 2 số"
+puts "Nhập A"
+a = gets
+puts "Nhập B"
+b = gets
+if a > b
+    puts "A lớn hơn B"
+else
+    puts "B lớn hơn A"
+end
